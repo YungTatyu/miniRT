e48eb76120286722214889c80de9f98969ed2d51
Tatsuki Terao <tterao@c1r23s9.42tokyo.jp>
2023-09-29 20:00:11 +0900

    init milibx

--- Makefile
+++ Makefile
@@ -3,9 +3,12 @@ CC = cc
 CFLAGS = -Wall -Werror -Wextra
 #CFLAGS += -g -fsanitize=address
 
-MINILIBX_DIR = minlibx-linux
-INCLUDES_DIR = includes
-INCLUDES =  -I$(INCLUDES_DIR) -I$(MINILIBX_DIR)
+MINILIBX_DIR = minilibx-linux
+INCLUDES_DIR = include
+X11_DIR = /usr/X11
+INCLUDES =  -I$(INCLUDES_DIR) -I$(MINILIBX_DIR) -I$(X11_DIR)/include
+MINILIBX_FLAGS = -L$(MINILIBX_DIR) -lmlx -L$(X11_DIR)/lib -lX11 -lXext
+MINILIBX_FILE = $(MINILIBX_DIR)/libmlx.a
 
 SRCS_DIR = src
 SRCS = $(SRCS_DIR)/main.c
@@ -18,13 +21,15 @@ RM = rm -rf
 all: $(NAME)
 
 $(NAME):	$(OBJS)
-	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $^
+	$(MAKE) -C $(MINILIBX_DIR)
+	$(CC) $(CFLAGS) $(MINILIBX_FLAGS) $(INCLUDES) -o $@ $^
 
 $(OBJS_DIR)/%.o: $(SRCS_DIR)/%.c
 	@mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 
 clean:
+	$(MAKE) -C $(MINILIBX_DIR) clean
 	$(RM) $(OBJS_DIR)
 
 fclean:	clean
--- src/main.c
+++ src/main.c
@@ -6,12 +6,21 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:54:04 by tterao            #+#    #+#             */
-/*   Updated: 2023/09/29 18:23:15 by tterao           ###   ########.fr       */
+/*   Updated: 2023/09/29 19:53:44 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include <stdlib.h>
+#include <mlx.h>
+
+typedef struct s_screen
+{
+	void	*mlx;
+	void	*mlx_win;
+	int		screen_width;
+	int		screen_height;
+}t_screen;
 
 
 int	main(int argc, char *argv[])
@@ -19,5 +28,15 @@ int	main(int argc, char *argv[])
 	if (argc == 1)
 		return (EXIT_FAILURE);
 	(void)argv;
+	t_screen data;
+	data.mlx = mlx_init();
+	data.screen_height = 1920;
+	data.screen_width = 1080;
+	data.mlx_win = mlx_new_window(data.mlx, data.screen_height, data.screen_width, "test");
+	while (1)
+	{
+		;
+	}
+
 	return (0);
 }
